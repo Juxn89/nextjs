6b7698f0d219fde69aa50d8188ac68c61d5b5756
Juan Gómez <gomezj01@stepstone.com>
2022-11-18 09:19:26 -0600

    Designe of pokemon page

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react'
 import { Layout } from '@components/layouts'
 import pokeAPI from '@api/pokeAPI'
 import { IPokemon } from '@interfaces/index'
 
 type PokemonPageProps = {
-    pokemon: any
+    pokemon: IPokemon
 }
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
@@ -33,13 +33,59 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   }
 
 const PokemonPage:NextPage<PokemonPageProps> = ({pokemon}) => {
-  const {query} = useRouter();
-
-  console.log(pokemon);
-
   return (
     <Layout title='Pokemon'>
-        <h1>{pokemon.name}</h1>
+      <Grid.Container css={{ marginTop:'5px' }} gap={ 2 }>
+        <Grid xs={ 12 } sm={ 4 }>
+          <Card isHoverable css={{ padding: '30px' }}>
+            <Card.Body>
+              <Card.Image  
+                src={ pokemon.sprites.other?.dream_world.front_default || 'no-image.png' }
+                alt={ pokemon.name }
+                width="100%"
+                height={ 200 }
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={ 12 } sm={ 8 }>
+          <Card>
+            <Card.Header css={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Text h1 transform='capitalize'> { pokemon.name } </Text>
+              <Button color='gradient' ghost> ⭐ Add to favorites</Button>
+            </Card.Header>
+            <Card.Body>
+              <Text size={30}>Sprites:</Text>
+              <Container direction='row' display='flex' gap={ 0 }>
+                <Image
+                  src={ pokemon.sprites.front_default }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height={ 100 }
+                />
+                <Image
+                  src={ pokemon.sprites.back_default }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height={ 100 }
+                />
+                <Image
+                  src={ pokemon.sprites.front_shiny }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height={ 100 }
+                />
+                <Image
+                  src={ pokemon.sprites.back_shiny }
+                  alt={ pokemon.name }
+                  width={ 100 }
+                  height={ 100 }
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </Layout>
   )
 }
