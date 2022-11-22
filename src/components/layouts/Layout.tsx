448aca53f10e939fcf3662217cf074ba2be176f1
Juan Gómez <gomezj01@stepstone.com>
2022-11-22 11:52:29 -0600

    Added Open Graph Meta tags

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
 import { Navbar } from '@components/ui';
+import { useRouter } from 'next/router';
 
 type LayoutProps = {
     children: JSX.Element | JSX.Element[],
     title?: string
 }
 
+const ORIGIN = (typeof window === 'undefined') ? '' : window.location.origin
+
 export const Layout = ({children, title}: LayoutProps) => {
+
   return (
     <>
         <Head>
@@ -15,6 +19,10 @@ export const Layout = ({children, title}: LayoutProps) => {
             <meta name="author" content="Juan Gómez"/>
             <meta name="description" content="Information about pokemon"/>
             <meta name="keywords" content={ `${title}, juan, gómez, juan gómez, pokemon, pokedex`}/>
+
+            <meta property="og:title" content={`Information about ${title}`} />
+            <meta property="og:description" content={`This page is about ${title}`} />
+            <meta property="og:image" content={`${ORIGIN}/img/banner_pokemon_seo.png`} />
         </Head>
 
         <Navbar />
