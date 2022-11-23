f52a5354e7a6e1a5155637c4dd82fab9c9ba4fcb
Juan Gómez <gomezj01@stepstone.com>
2022-11-23 09:28:25 -0600

    Incremental Static Generation (ISG)

--- src/utils/getPokemonInfo.ts
+++ src/utils/getPokemonInfo.ts
@@ -2,11 +2,15 @@ import { pokeAPI } from "@api/index";
 import { IPokemon } from "@interfaces/index";
 
 export const getPokemonInfo = async (nameOrId: string) => {
-    const { data } = await  pokeAPI.get<IPokemon>(`/pokemon/${ nameOrId }`)
-
-    return { 
-        id: data.id, 
-        name: data.name, 
-        sprites: data.sprites 
+    try {
+        const { data } = await  pokeAPI.get<IPokemon>(`/pokemon/${ nameOrId }`)
+        
+        return { 
+            id: data.id, 
+            name: data.name, 
+            sprites: data.sprites 
+        }
+    } catch (error) {
+        return null;
     }
 }
