5e526df6cb859fd29891dcdfe203d48ca70b79c4
Juan Gómez <gomezj01@stepstone.com>
2022-11-30 12:11:53 -0600

    Save a entry in the database

--- src/pages/api/entries/index.ts
+++ src/pages/api/entries/index.ts
@@ -6,11 +6,14 @@ import { IEntry } from "@interfaces/index";
 type EntriesResponseType = 
 	| { name: string }
 	| IEntry[]
+	| IEntry
 
 const handler = (req: NextApiRequest, res: NextApiResponse<EntriesResponseType>) => {
 	switch (req.method) {
 		case 'GET':			
 			return getEntries(res);
+		case 'POST':
+			return postEntry(req, res);
 		default:
 			return res.status(400).json({name: 'Enpoint doesn\'t exist'});
 	}
@@ -25,6 +28,27 @@ const getEntries = async (res: NextApiResponse<EntriesResponseType>) => {
 	await db.disconnect();
 
 	res.status(200).json(entries);
+}
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<EntriesResponseType>) => {
+	const { description = '' } = req.body;
+	
+	const newEntry = new Entry({
+		description,
+		createdAt: Date.now()
+	})
+
+	try {
+			await db.connect()
+			await newEntry.save()
+			await db.disconnect()
+
+			return res.status(201).json(newEntry)
+	} catch (error) {
+		await db.disconnect()
+		console.log(error);
+		return res.status(500).json({name: 'Something was wrong, check browser console'})
+	}
 }
 
 export default handler
