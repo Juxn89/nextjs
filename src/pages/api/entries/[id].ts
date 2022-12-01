9eba249f962e84990b6b1e8a5fd8dd8c6fff477e
Juan Gómez <gomezj01@stepstone.com>
2022-12-01 13:26:24 -0600

    Get entry by ID

--- src/pages/api/entries/[id].ts
+++ src/pages/api/entries/[id].ts
@@ -12,9 +12,11 @@ const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
 	const { id } = req.query;
 
 	if(!mongoose.isValidObjectId(id))
-		return res.status(400).json({ message: 'ID is not valid' });
+		return res.status(400).json({ message: `Entry with ID ${id} was not found!` });
 
 	switch (req.method) {
+		case 'GET':
+			return getEntry(req, res);
 		case 'PUT':
 			return updateEntry(req, res);
 		default:
@@ -54,6 +56,23 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse) => {
 		await db.disconnect()		
 		return res.status(200).json(
 			{ message: `Something is wrong! \n ${error.errors.status}`});		
+	}
+}
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse) => {
+	const { id } = req.query;
+
+	try {
+		await db.connect()
+
+		const entry = await Entry.findById(id);
+
+		await db.disconnect();
+
+		return res.status(200).json(entry);
+
+	} catch (error) {
+		return res.status(500).json(`Something is wrong!`);		
 	}
 }
 
