56f8a0e87f3f77dd047373adebb73406f53506be
Juan Gómez <gomezj01@stepstone.com>
2022-12-13 14:25:02 -0600

    Set theme from cookie - frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,35 @@
-import { GetServerSideProps } from 'next'
+import { useEffect, useState } from 'react'
 import type { AppContext, AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { darkTheme } from '@theme/index'
+import { CssBaseline, ThemeProvider, Theme } from '@mui/material'
+import { customTheme, darkTheme, lightTheme } from '@theme/index'
 import '@styles/globals.css'
+import Cookies from 'js-cookie';
 
-export default function App({ Component, pageProps, ...rest }: AppProps) {
-  console.log({...rest});
+interface Props extends AppProps {
+  theme: string
+}
+
+export default function App({ Component, pageProps, theme }: Props) {
+
+  const [currentTheme, setCurrentTheme] = useState(lightTheme)
+
+  useEffect(() => {
+    const cookieTheme = Cookies.get('theme')
+
+    const selectedTheme: Theme = theme === 'light' 
+    ? lightTheme 
+    : theme === 'dark' 
+      ? darkTheme 
+      : customTheme
+
+      setCurrentTheme(selectedTheme)
+  }, [])
+  
+
+
+
   return (
-    <ThemeProvider theme={ darkTheme }>
+    <ThemeProvider theme={ currentTheme }>
       <CssBaseline>
         <Component {...pageProps} />
       </CssBaseline>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,1 +1,3 @@
+export * from '@theme/light-theme'
 export * from '@theme/darkTheme'
+export * from '@theme/custom-theme'
