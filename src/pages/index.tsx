3810ac23bf23f487ef2260ac6e752badc177747b
Juan Gómez <gomezj01@stepstone.com>
2022-12-14 10:00:04 -0600

    ShopLayout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { ShopLayout } from '@components/layouts/ShopLayout';
+import { Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <>
-      <h1>TesloShop</h1>
-    </>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Find the best Teslo\'s products here'}>
+      <Typography variant='h1' component='h1'>Shop</Typography>
+      <Typography variant='h2' sx={{ marginBottom: 1 }}>All products</Typography>
+    </ShopLayout>
   )
 }
