ed72711ecfe90551795a8bf41129d8fb86758915
Juan Gómez <gomezj01@stepstone.com>
2022-12-16 14:34:52 -0600

    Home page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { ShopLayout } from '@components/layouts/ShopLayout';
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { initialData } from '@database/products'
 
 export default function Home() {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Find the best Teslo\'s products here'}>
       <Typography variant='h1' component='h1'>Shop</Typography>
       <Typography variant='h2' sx={{ marginBottom: 1 }}>All products</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(item => (            
+            <Grid key={item.slug} item xs={6} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component='img' image={ `products/${ item.images[0] }` } alt={ item.title }></CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
